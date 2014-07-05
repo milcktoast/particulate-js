87eb09d56f05ae060b3c2a8927bf3bc01132e2e8
Jay Weeks <jay.weeks@ff0000.com>
2014-07-05 11:18:33 -0700

    Add PointForce types

--- src/forces/PointForce.js
+++ src/forces/PointForce.js
@@ -1,10 +1,16 @@
 require('./Force');
 lib.PointForce = PointForce;
-function PointForce(x, y, z, radius, intensity) {
+function PointForce(x, y, z, opts) {
+  opts = opts || {};
   lib.Force.apply(this, arguments);
-  this.intensity = intensity || 0.05;
-  this.setRadius(radius || 0);
+  this.type = opts.type || PointForce.ATTRACTOR;
+  this.intensity = opts.intensity || 0.05;
+  this.setRadius(opts.radius || 0);
 }
+
+var pf_ATTRACTOR = PointForce.ATTRACTOR = 0;
+var pf_REPULSOR = PointForce.REPULSOR = 1;
+var pf_ATTRACTOR_REPULSOR = PointForce.ATTRACTOR_REPULSOR = 2;
 
 PointForce.prototype = Object.create(lib.Force.prototype);
 
@@ -14,7 +20,6 @@ PointForce.prototype.setRadius = function (r) {
 
 PointForce.prototype.applyForce = function (i, f0, p0) {
   var v0 = this.vec;
-  var r2 = this._radius2;
   var ix = i;
   var iy = ix + 1;
   var iz = ix + 2;
@@ -23,11 +28,24 @@ PointForce.prototype.applyForce = function (i, f0, p0) {
   var dy = p0[iy] - v0[1];
   var dz = p0[iz] - v0[2];
 
-  var dist, scale;
+  var dist = dx * dx + dy * dy + dz * dz;
+  var diff = dist - this._radius2;
+  var isActive, scale;
 
-  if (dx || dy || dz) {
-    dist = dx * dx + dy * dy + dz * dz;
-    scale = (dist - this._radius2) / dist * this.intensity;
+  switch (this.type) {
+  case pf_ATTRACTOR:
+    isActive = dist > 0 && diff > 0;
+    break;
+  case pf_REPULSOR:
+    isActive = dist > 0 && diff < 0;
+    break;
+  case pf_ATTRACTOR_REPULSOR:
+    isActive = dx || dy || dz;
+    break;
+  }
+
+  if (isActive) {
+    scale = diff / dist * this.intensity;
 
     f0[ix] -= dx * scale;
     f0[iy] -= dy * scale;
