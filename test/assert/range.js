bbc336af8932a08bc4add8c8d890c9a1e1cc1900
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-07-26 14:29:27 -0700

    Add BoxConstraint tests

---
+++ test/assert/range.js
@@ -0,0 +1,6 @@
+Test.assert.range = assert_range;
+function assert_range(actual, min, max, message) {
+  var passes = actual >= min && actual <= max;
+  var expected = [min, max];
+  QUnit.push(passes, actual, expected, message);
+}
