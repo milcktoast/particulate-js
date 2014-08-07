34cd33a3fcb2427af27bf5e5a009c69e6d144122
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-08-06 18:39:56 -0700

    Add DistanceConstraint indices argument
    Enable constraining multiple sets of particles per instance

--- src/constraints/DistanceConstraint.js
+++ src/constraints/DistanceConstraint.js
@@ -1,9 +1,11 @@
 require('./Constraint');
 lib.DistanceConstraint = DistanceConstraint;
 function DistanceConstraint(distance, a, b) {
-  lib.Constraint.call(this, 2);
+  var size = a.length || arguments.length - 1;
+  lib.Constraint.call(this, size);
   this.setDistance(distance);
   this.setIndices(a, b);
+  this._count = size * 0.5;
 }
 
 DistanceConstraint.create = lib.ctor(DistanceConstraint);
@@ -13,9 +15,7 @@ DistanceConstraint.prototype.setDistance = function (distance) {
   this._distance2 = distance * distance;
 };
 
-DistanceConstraint.prototype.applyConstraint = function (p0, p1, w0) {
-  var ii = this.indices;
-  var ai = ii[0], bi = ii[1];
+function dc_applyConstraint(p0, p1, w0, dist2, ai, bi) {
   var ax = ai * 3, ay = ax + 1, az = ax + 2;
   var bx = bi * 3, by = bx + 1, bz = bx + 2;
 
@@ -31,7 +31,6 @@ DistanceConstraint.prototype.applyConstraint = function (p0, p1, w0) {
   var bw = w0[bi];
   var tw = aw + bw;
 
-  var dist2 = this._distance2;
   var len2 = dx * dx + dy * dy + dz * dz;
   var diff = dist2 / (len2 + dist2);
 
@@ -45,4 +44,22 @@ DistanceConstraint.prototype.applyConstraint = function (p0, p1, w0) {
   p0[bx] += dx * bDiff;
   p0[by] += dy * bDiff;
   p0[bz] += dz * bDiff;
+}
+
+DistanceConstraint.prototype.applyConstraint = function (p0, p1, w0) {
+  var dist2 = this._distance2;
+  var count = this._count;
+  var ii = this.indices;
+
+  if (count === 1) {
+    dc_applyConstraint(p0, p1, w0, dist2, ii[0], ii[1]);
+    return;
+  }
+
+  var i, ai, bi;
+  for (i = 0; i < count; i ++) {
+    ai = i * 2;
+    bi = ai + 1;
+    dc_applyConstraint(p0, p1, w0, dist2, ii[ai], ii[bi]);
+  }
 };
--- test/constraints/DistanceConstraint.js
+++ test/constraints/DistanceConstraint.js
@@ -8,24 +8,37 @@ var DistanceConstraint = Particulate.DistanceConstraint;
 test('Creation', function () {
   var dist = 2;
   var a = 1, b = 3;
-  var constraint = DistanceConstraint.create(dist, a, b);
+  var indices = [0, 1, 2, 3];
+  var fromArgs = DistanceConstraint.create(dist, a, b);
+  var fromArray = DistanceConstraint.create(dist, indices);
 
-  equal(constraint._distance2, dist * dist,
+  equal(fromArgs._distance2, dist * dist,
     'Should initialize precalculated distance^2.');
+  Test.assert.equalArray(fromArgs.indices, [a, b],
+    'Should create indices from int arguments.');
+  Test.assert.equalArray(fromArray.indices, indices,
+    'Should create indices from int array.');
 });
 
 // Application
 // -----------
 
 test('Application', function () {
-  var system = Particulate.ParticleSystem.create(4, 10);
+  var system = Particulate.ParticleSystem.create(6, 10);
   var dist = 2;
-  var a = 1, b = 3;
-  var constraint = DistanceConstraint.create(dist, a, b);
+  var a = 0, b = 1;
+  var c = 2, d = 3, e = 4, f = 5;
+  var single = DistanceConstraint.create(dist, a, b);
+  var many = DistanceConstraint.create(dist, [c, d, e, f]);
 
-  system.addConstraint(constraint);
+  system.addConstraint(single);
+  system.addConstraint(many);
   system.tick(1);
 
   Test.assert.close(system.getDistance(a, b), dist, 0.1,
-    'Should move particles toward constraint\'s distance.');
+    'Should move particles [a, b] toward constraint\'s distance.');
+  Test.assert.close(system.getDistance(c, d), dist, 0.1,
+    'Should move particles [c, d] toward constraint\'s distance.');
+  Test.assert.close(system.getDistance(e, f), dist, 0.1,
+    'Should move particles [e, f] toward constraint\'s distance.');
 });
