3142200706548a189b0110de1da9a4822a838e24
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-08-30 15:44:15 -0700

    Add AngleConstraint tests

--- test/constraints/AngleConstraint.js
+++ test/constraints/AngleConstraint.js
@@ -1,6 +1,7 @@
 module('Constraint.Angle');
 
 var AngleConstraint = Particulate.AngleConstraint;
+var PI = Math.PI;
 
 // Creation
 // --------
@@ -22,15 +23,10 @@ test('Creation', function () {
 // -----------
 
 function testAtAngle(angle) {
-  var system = Particulate.ParticleSystem.create(9, 10);
-  var dist = Particulate.DistanceConstraint.create(2, [0, 1, 1, 2]);
+  var system = Particulate.ParticleSystem.create(3, 50);
   var single = AngleConstraint.create(angle, 0, 1, 2);
 
-  system.setPosition(0, [1, 0, 0]);
-  system.setPosition(1, [2, 2, 0]);
-  system.setPosition(2, [3, 0, 0]);
-
-  system.addConstraint(dist);
+  system.perturb(0.5);
   system.addConstraint(single);
   system.tick(1);
 
@@ -39,9 +35,36 @@ function testAtAngle(angle) {
     'Should constrain particles to ' + approxAngle + ' radians.');
 }
 
-test('Application of angles', function () {
+test('Application of angle', function () {
   var angles = 10;
   for (var i = 0; i < angles; i ++) {
-    testAtAngle(Math.PI * i / (angles - 1));
+    testAtAngle(PI * i / (angles - 1));
   }
+});
+
+test('Application of angle range', function () {
+  var system = Particulate.ParticleSystem.create(9, 50);
+  var noMin = AngleConstraint.create([0, PI * 0.25],         0, 1, 2);
+  var noMax = AngleConstraint.create([PI * 0.25, Infinity],  3, 4, 5);
+  var range = AngleConstraint.create([PI * 0.25, PI * 0.75], 6, 7, 8);
+  var tolerance = 0.1;
+
+  system.setPosition(0, [  0, 10, 0]);
+  system.setPosition(2, [ 10,  0, 0]);
+  system.setPosition(3, [ 10,  2, 0]);
+  system.setPosition(5, [ 10,  1, 0]);
+  system.setPosition(6, [-10,  0, 0]);
+  system.setPosition(8, [ 10,  0, 0]);
+
+  system.addConstraint(noMin);
+  system.addConstraint(noMax);
+  system.addConstraint(range);
+  system.tick(1);
+
+  Test.assert.range(system.getAngle(0, 1, 2), noMin._min - tolerance, noMin._max + tolerance,
+    'Should constrain particles if angle is greater than upper bound of range.');
+  Test.assert.range(system.getAngle(3, 4, 5), noMax._min - tolerance, noMax._max + tolerance,
+    'Should constrain particles if angle is less than lower bound of range.');
+  Test.assert.range(system.getAngle(6, 7, 8), range._min - tolerance, range._max + tolerance,
+    'Should not affect particles within range tolerance.');
 });
