1590155ef9d592baa1b37d565d70d913dec756da
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-08-30 15:53:36 -0700

    Update Vec3
    - add length, distance
    - fix angle

--- src/math/Vec3.js
+++ src/math/Vec3.js
@@ -28,6 +28,35 @@ Vec3.get = function (b0, i, out) {
   return out;
 };
 
+Vec3.lengthSq = function (b0, ai) {
+  var aix = ai * 3, aiy = aix + 1, aiz = aix + 2;
+  var x = b0[aix];
+  var y = b0[aiy];
+  var z = b0[aiz];
+
+  return x * x + y * y + z * z;
+};
+
+Vec3.length = function (b0, ai) {
+  var aix = ai * 3, aiy = aix + 1, aiz = aix + 2;
+  var x = b0[aix];
+  var y = b0[aiy];
+  var z = b0[aiz];
+
+  return Math.sqrt(x * x + y * y + z * z);
+};
+
+Vec3.distanceSq = function (b0, ai, bi) {
+  var aix = ai * 3, aiy = aix + 1, aiz = aix + 2;
+  var bix = bi * 3, biy = bix + 1, biz = bix + 2;
+
+  var dx = b0[aix] - b0[bix];
+  var dy = b0[aiy] - b0[biy];
+  var dz = b0[aiz] - b0[biz];
+
+  return dx * dx + dy * dy + dz * dz;
+};
+
 Vec3.distance = function (b0, ai, bi) {
   var aix = ai * 3, aiy = aix + 1, aiz = aix + 2;
   var bix = bi * 3, biy = bix + 1, biz = bix + 2;
@@ -44,18 +73,18 @@ Vec3.angle = function (b0, ai, bi, ci) {
   var bix = bi * 3, biy = bix + 1, biz = bix + 2;
   var cix = ci * 3, ciy = cix + 1, ciz = cix + 2;
 
-  var abLen = 1 / Vec3.distance(b0, ai, bi);
+  var baLen = 1 / Vec3.distance(b0, bi, ai);
   var bcLen = 1 / Vec3.distance(b0, bi, ci);
 
-  var abX = (b0[bix] - b0[aix]) * abLen;
-  var abY = (b0[biy] - b0[aiy]) * abLen;
-  var abZ = (b0[biz] - b0[aiz]) * abLen;
+  var baX = (b0[aix] - b0[bix]) * baLen;
+  var baY = (b0[aiy] - b0[biy]) * baLen;
+  var baZ = (b0[aiz] - b0[biz]) * baLen;
 
   var bcX = (b0[cix] - b0[bix]) * bcLen;
   var bcY = (b0[ciy] - b0[biy]) * bcLen;
   var bcZ = (b0[ciz] - b0[biz]) * bcLen;
 
-  var dot = abX * bcX + abY * bcY + abZ * bcZ;
+  var dot = baX * bcX + baY * bcY + baZ * bcZ;
 
   return Math.acos(dot);
 };
