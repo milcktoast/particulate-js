6432ae4a45bb9c8e639d55cedd2cd9fc6482ea07
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-12-01 18:46:58 -0800

    Add Creator docs

--- src/utils/Creator.js
+++ src/utils/Creator.js
@@ -1,3 +1,16 @@
+/**
+  @class Particulate
+*/
+
+/**
+  Functional constructor utility.
+
+  @method ctor
+  @param  {Function} Ctor         Constructor function used to instantiate class
+  @return {Function} constructor
+  @private
+  @static
+*/
 lib.ctor = function ctor(Ctor) {
   return function () {
     var instance = Object.create(Ctor.prototype);
