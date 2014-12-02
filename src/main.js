9dd6c54c4b4892e6a5446b96b3c62b2fd67786da
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-12-01 18:47:29 -0800

    Update main header comment

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-// Particulate.js 0.3.1
-// ====================
+// ..................................................
+// Particulate.js
+//
+// version : 0.3.1
+// authors : Jay Weeks
+// license : MIT
+// particulatejs.org
+// ..................................................
 
 (function () {
   'use strict';
-  var lib = {VERSION : '0.3.1'};
+
+  var lib = {
+    VERSION : '0.3.1'
+  };
+
   require('utils/*');
   require('math/*');
   require('forces/*');
   require('constraints/*');
   require('systems/*');
+
+  /**
+    @class Particulate
+    @static
+  */
   this.Particulate = lib;
 }).call(this);
