bb5498ad2cd1ad186c64e6a35a77598dc9b3b3e6
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-12-05 10:21:32 -0800

    Add built docs

---
+++ docs/api.js
@@ -0,0 +1,53 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AngleConstraint",
+        "AxisConstraint",
+        "BoundingPlaneConstraint",
+        "BoxConstraint",
+        "Collection",
+        "Constraint",
+        "DirectionalForce",
+        "DistanceConstraint",
+        "Force",
+        "Math",
+        "ParticleSystem",
+        "Particulate",
+        "PlaneConstraint",
+        "PointConstraint",
+        "PointForce",
+        "Vec3"
+    ],
+    "modules": [
+        "constraints",
+        "forces",
+        "math",
+        "systems",
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "constraints",
+            "name": "constraints",
+            "description": "Constraints define relationships between multiple particles or\nbetween particles and geometric primitives."
+        },
+        {
+            "displayName": "forces",
+            "name": "forces",
+            "description": "Forces are accumulated and applied to particles, affecting their\nacceleration and velocity in the system's integration step."
+        },
+        {
+            "displayName": "math",
+            "name": "math"
+        },
+        {
+            "displayName": "systems",
+            "name": "systems"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils"
+        }
+    ]
+} };
+});
