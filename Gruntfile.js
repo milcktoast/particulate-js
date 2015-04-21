2a8c4f9a05e30f42a808cb14dbb75f0c60de6ec7
Jay Weeks <jay.patrick.weeks@gmail.com>
2015-04-21 15:36:05 -0700

    Add jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,9 @@ module.exports = function (grunt) {
     lib: 'node_modules/'
   };
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-yuidoc');
-  grunt.loadNpmTasks('grunt-qunit-istanbul');
-  grunt.loadNpmTasks('grunt-coveralls');
-  grunt.loadNpmTasks('grunt-neuter');
+  require('jit-grunt')(grunt, {
+    qunit: 'grunt-qunit-istanbul'
+  });
 
   grunt.initConfig({
     jshint: {
